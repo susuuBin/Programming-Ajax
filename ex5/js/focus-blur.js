0e45b92495b64f52fc7d4568e458caef838f24ea
susuuBin <azxq1000@gmail.com>
2020-05-14 10:21:46 +0900

    :fire: 5 월 14 일, 프로그래밍 (Ajax) 수업 소스.

---
+++ ex5/js/focus-blur.js
@@ -0,0 +1,20 @@
+function checkUsername(){
+    var username = el.value;
+    if (username.length < 5){
+        elMsg.className = 'warning';
+        elMsg.textContent = '이름이 너무 짧습니다.';
+    } else {
+        elMsg.textContent = '';
+    }
+}
+
+function tipUsername(){
+    elMsg.className = 'tip';
+    elMsg.innerHTML = '이름은 5 글자 이상이어야 합니다.';
+}
+
+var el = document.getElementById('username');
+var elMsg = document.getElementById('feedback');
+
+el.addEventListener('focus', tipUsername, false);
+el.addEventListener('blur', checkUsername, false);
