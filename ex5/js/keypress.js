e704876a3d2fbc3035213966d24f071ba4d479c3
susuuBin <azxq1000@gmail.com>
2020-05-21 10:43:59 +0900

    5 월 21 일, 프로그래밍 (Ajax) 수업 코드.

---
+++ ex5/js/keypress.js
@@ -0,0 +1,15 @@
+var el;
+
+function charCount(e){
+    var textEntered, charDisplay, counter, lastkey;
+    textEntered = document.getElementById('message').value;
+    charDisplay = document.getElementById('charactersLeft');
+    counter = (180 - (textEntered.length));
+    charDisplay.textContent = counter;
+
+    lastkey = document.getElementById('lastKey');
+    lastkey.textContent = '마지막 입력의 ASCII 코드 : ' + e.keyCode + ' 자';
+}
+
+el = document.getElementById('message');
+el.addEventListener('keypress', charCount, false);
